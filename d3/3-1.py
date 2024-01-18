fc87114c9c3d2a47a6006a1f1167b9f943712407
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-01-18 11:52:59 +0530

    Create 3-1.py

---
+++ d3/3-1.py
@@ -0,0 +1,28 @@
+# python comments and escape sequences
+
+import pandas
+
+
+# single line comment
+# print ("hello") 
+print ("hello world")
+
+
+# multiline comments - use single Quotation mark
+
+'''
+print("1")
+print("2")
+print("3")
+print("4")
+'''
+
+# for printing in double Quotation mark ex: hello my name is "jay"
+# use \" 
+print("hello my name is \"jay\" ")
+
+
+# how to get new line for print statementn 
+# escape sequences  
+# technique - 1 : use "\n" 
+print("Welcome to day-4 \nand today is Tuesday")
