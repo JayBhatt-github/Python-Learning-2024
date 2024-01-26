d32f239be22a4e59c938ff6d9416e91e087ec06a
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-01-26 14:58:58 +0530

    Create 36_if_name_==__main__.py

---
+++ Python/36_if_name_==__main__.py
@@ -0,0 +1,13 @@
+# What does the if __name__ == “__main__”: do in python ?
+
+
+# The if __name__ == "__main__": statement in Python is used to check whether the current script is being run directly or being imported as a module into another script. 
+# When the script is run directly, the __name__ variable is set to "__main__", and the code block inside the if statement will be executed. 
+# This is commonly used to define the behavior of a script when it is run directly, such as running tests or performing specific tasks. 
+# It allows the same script to be used both as a standalone program and as a module in another program.
+if __name__ == "__main__":
+    # Code to be executed when the script is run directly
+    print("This script is being run directly")
+    # Additional code can be added here to perform specific tasks when the script is run directly
+
+    
