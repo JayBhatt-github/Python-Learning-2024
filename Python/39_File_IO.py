6654bf3e7d49816674f87d5c7333b73fc5668fce
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-01-27 11:18:22 +0530

    .

---
+++ Python/39_File_IO.py
@@ -0,0 +1,40 @@
+# file io in python
+
+# Open a file in write mode
+# file = open("example.txt", "w")
+
+# Write to the file
+# file.write("Hello, this is a demonstration of file IO in Python.")
+
+# Close the file
+# file.close()
+
+# Open the file in read mode
+# file = open("example.txt", "r")
+
+# Read from the file
+# print(file.read())
+
+# Close the file
+# file.close()
+
+
+
+
+# Other Example 
+
+# READING A FILE
+# f = open('myfile.txt', 'r')
+# # print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
+
+# WRITING A FILE
+# f = open('myfile.txt', 'a')    # "a" --> appends
+# f.write('Hello, world!')
+# f.close()
+
+# with open('myfile.txt', 'a') as f:
+#   f.write("Hey I am inside with")
