ebccd95c1cf53808e130896932d6b54325f2320c
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-01-27 11:44:33 +0530

    .

---
+++ Python/41_seek()_and_tell()_function.py
@@ -0,0 +1,17 @@
+# # Open a file in read mode
+# file = open("example.txt", "r")
+
+# # Read the first 10 characters
+# print("Reading the first 10 characters:", file.read(10))
+
+# # Use tell() to get the current position
+# print("Current position:", file.tell())
+
+# # Use seek() to move the cursor to the beginning
+# file.seek(0)
+
+# # Read the file from the new position
+# print("Reading the file from the beginning:", file.read())
+
+# # Close the file
+# file.close()
