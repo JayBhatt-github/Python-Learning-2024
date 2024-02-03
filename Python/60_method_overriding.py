2fcb155a5624f070a851e0b342194dfd83a69260
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-02-03 12:37:24 +0530

    Create 60_method_overriding.py

---
+++ Python/60_method_overriding.py
@@ -0,0 +1,40 @@
+# Method Overriding in Python
+
+class Parent:
+    def show(self):
+        print("Parent's show method")
+
+class Child(Parent):
+    def show(self):
+        print("Child's show method")
+
+# Creating object of child class
+obj = Child()
+
+# Call the show method of child class
+obj.show()
+
+
+
+# Other Example
+class Shape:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    
+  def area(self):
+      return self.x * self.y
+
+class Circle(Shape):
+    def __init__(self, radius):
+      self.radius = radius
+      super().__init__(radius, radius)
+
+    def area(self):
+        return f"circle is {3.14 * super().area()}"
+      
+rec = Shape(3, 5)
+print(rec.area())
+
+c = Circle(5)
+print(c.area())
