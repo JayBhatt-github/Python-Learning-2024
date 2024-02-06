053fdc2376f4f23b95877b38ec62d6d1660858a8
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2024-02-06 20:44:22 +0530

    .

---
+++ Python/70_shutil_module.py
@@ -0,0 +1,43 @@
+# SH Util Module in Python
+
+import shutil
+
+
+# Copy a file from one location to another
+# shutil.copy('source_file', 'destination_file')
+
+# Copy a directory from one location to another
+# shutil.copytree('source_directory', 'destination_directory')
+
+# Move a file from one location to another
+# shutil.move('source_file', 'destination_file')
+
+# Move a directory from one location to another
+# shutil.move('source_directory', 'destination_directory')
+
+# Rename a file
+# shutil.move('old_file_name', 'new_file_name')
+
+# Delete a file
+# shutil.rmtree('file_to_delete')
+
+# Delete a directory
+# shutil.rmtree('directory_to_delete')
+
+# Create a zip file
+# shutil.make_archive('archive_name', 'zip', 'directory_to_zip')
+
+# Extract a zip file
+# shutil.unpack_archive('archive_name', 'directory_to_extract_to')
+
+# Get the disk usage of a directory
+# shutil.disk_usage('directory_path')
+
+# Get the total size of a directory
+# shutil.disk_usage('directory_path').total
+
+# Get the used space of a directory
+# shutil.disk_usage('directory_path').used
+
+# Get the free space of a directory
+# shutil.disk_usage('directory_path').free
